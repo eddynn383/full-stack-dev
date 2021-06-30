fdf4095c574a65702619a200c95b2447ee104902
Eduard Boboc <eddynn383@gmail.com>
2021-07-01 00:19:25 +0300

    Push final edits

---
+++ public/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.slider_items').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+})
